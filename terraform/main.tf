d54b20e3e5cb19fdbf5ce7fa72e811f007eb0e4a
Rubi <rubi.cloud87@gmail.com>
2024-06-20 10:53:28 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,20 @@ resource "aws_instance" "strapi" {
   tags = {
     Name = "StrapiServer"
   }
-
+  user_data                   = <<-EOF
+                                #!/bin/bash
+                                sudo apt update
+                                curl -fsSL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
+                                sudo bash -E nodesource_setup.sh
+                                sudo apt update && sudo apt install nodejs -y
+                                sudo npm install -g yarn && sudo npm install -g pm2
+                                echo -e "skip\n" | npx create-strapi-app simple-strapi --quickstart
+                                cd simple-strapi
+                                echo "const strapi = require('@strapi/strapi');
+                                strapi().start();" > server.js
+                                pm2 start server.js
+                                sleep 360
+                                EOF
 
   key_name = "strapi_key2"
 }
