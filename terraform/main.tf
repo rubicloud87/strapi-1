dd28563c102a4119519c58e60ecf9408ce45ec04
Rubi <rubi.cloud87@gmail.com>
2024-06-20 11:47:55 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,8 @@ resource "aws_instance" "strapi" {
   ami                    = "ami-09040d770ffe2224f" # Amazon Linux 2 AMI
   instance_type          = "t2.medium"
   subnet_id              = "subnet-0f8632da3b474a417"
-  vpc_security_group_ids = [aws_security_group.Strapi_SG.id]
+  vpc_security_group_ids = [aws_security_group.Strapi_sg.id]
+  key_name = "strapi_key2"
   tags = {
     Name = "StrapiServer"
   }
@@ -23,13 +24,13 @@ resource "aws_instance" "strapi" {
                                 sleep 360
                                 EOF
 
-  key_name = "strapi_key2"
+ 
 }
 
 
 
-resource "aws_security_group" "Strapi_SG" {
-  name        = "Strapi_SG"
+resource "aws_security_group" "Strapi_sg" {
+  name        = "Strapi_sg"
   description = "Strapi_SG"
 
   vpc_id = "vpc-0d4da62efad50eb46" # Replace with your VPC ID
